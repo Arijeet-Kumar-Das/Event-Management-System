14e238caac97533b24c70d1b995192137e88441e
Arijeet Kumar Das <dasarijeetkumar@gmail.com>
2023-03-20 17:40:01 +0530

    second change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import ArticleIcon from "@mui/icons-material/Article";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
@@ -22,9 +22,14 @@ import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
+import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
 import event1 from "../Images/event-1.jpg";
 import event2 from "../Images/event-2.jpg";
 import event3 from "../Images/event-3.jpg";
+import Rating from "@mui/material/Rating";
+import TravelExploreIcon from "@mui/icons-material/TravelExplore";
+import PublicIcon from "@mui/icons-material/Public";
+import KitesurfingIcon from "@mui/icons-material/Kitesurfing";
 const Home = () => {
   const PaperStyle = {
     display: "flex",
@@ -33,6 +38,7 @@ const Home = () => {
     alignItems: "center",
     height: "150px",
   };
+  const [value, setValue] = useState(2);
   return (
     <div>
       <Box
@@ -58,10 +64,13 @@ const Home = () => {
               color: "white",
               textAlign: "center",
               fontSize: { xs: "30px", md: "50px" },
+              marginTop: "30px",
             }}
           >
             Connecting the World
-            <Typography variant="h6">Plan Your Next Event</Typography>
+          </Typography>
+          <Typography variant="h6" sx={{ color: "white" }}>
+            Plan Your Next Event
           </Typography>
           <Box sx={{ display: { xs: "none", md: "flex" } }}>
             <TextField
@@ -115,6 +124,10 @@ const Home = () => {
                 borderRadius: "0",
                 color: "black",
                 p: "0 30px",
+
+                "&:hover": {
+                  backgroundColor: "#a62b2b",
+                },
               }}
             >
               Search
@@ -271,102 +284,299 @@ const Home = () => {
         </Grid>
       </Box>
       <Box marginTop="100px" backgroundColor="#e3dddd" padding="20px 50px">
-        <Typography variant="h5" sx={{ textAlign: "center" }}>
+        <Typography
+          variant="h5"
+          sx={{
+            textAlign: "center",
+            fontSize: "2rem",
+          }}
+        >
           Upcoming Events
         </Typography>
-
+        <Box
+          sx={{
+            backgroundColor: "red",
+            margin: "auto",
+            height: "3  px",
+            width: "100px",
+          }}
+        ></Box>
         <Box
           display="flex"
           sx={{ flexDirection: { xs: "column", md: "row" } }}
           gap={5}
           justifyContent="center"
           alignItems="center"
-          marginTop="20px"
+          marginTop="50px"
         >
           <Card sx={{ maxWidth: 300, maxheight: 400 }}>
-            <CardMedia
-              sx={{ height: 160 }}
-              image={event1}
-              title="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Cycling Competition
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Pariatur reiciendis molestiae Lorem ipsum dolor sit amet,
-                consectetur
-              </Typography>
+            <CardMedia sx={{ height: 160 }} image={event1} title="event" />
+            <CardContent
+              sx={{ display: "flex", flexDirection: "column", gap: 2 }}
+            >
+              <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+                <Button
+                  size="small"
+                  sx={{
+                    backgroundColor: "red",
+                    color: "white",
+                    "&:hover": {
+                      backgroundColor: "orange",
+                    },
+                  }}
+                >
+                  Sports
+                </Button>
+                <Button size="small">Price : $12</Button>
+              </Box>
+              <Box>
+                <Typography variant="h6">
+                  A Cycling competition for every Age
+                </Typography>
+                <Box display="flex" flexDirection="column" marginTop="10px">
+                  <Box display="flex" alignItems="center" gap={2}>
+                    <AccessAlarmIcon />
+                    <Typography variant="p">
+                      Thu, September 5, 2023, 12:00pm
+                    </Typography>
+                  </Box>
+                  <Box display="flex" alignItems="center" gap={2}>
+                    <LocationOnIcon />
+                    <Typography variant="p">City Center, Guwahati</Typography>
+                  </Box>
+                </Box>
+              </Box>
             </CardContent>
             <CardActions
-              sx={{ display: "flex", justifyContent: "space-between" }}
+              sx={{ display: "flex", justifyContent: "space-around" }}
             >
-              <Button
-                size="small"
-                sx={{ backgroundColor: "red", color: "white" }}
-              >
-                Book Tickets
-              </Button>
-              <Button size="small">Price : $12</Button>
+              <Rating
+                name="simple-controlled"
+                value={value}
+                onChange={(event, newValue) => {
+                  setValue(newValue);
+                }}
+              />
+              <Button sx={{ border: "1px solid black" }}>Book Tickets</Button>
             </CardActions>
           </Card>
           <Card sx={{ maxWidth: 300, maxheight: 400 }}>
-            <CardMedia
-              sx={{ height: 160 }}
-              image={event2}
-              title="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Singing Competition
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Pariatur reiciendis molestiae Lorem ipsum dolor sit amet,
-                consectetur
-              </Typography>
+            <CardMedia sx={{ height: 160 }} image={event2} title="event2" />
+            <CardContent
+              sx={{ display: "flex", flexDirection: "column", gap: 2 }}
+            >
+              <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+                <Button
+                  size="small"
+                  sx={{
+                    backgroundColor: "purple",
+                    color: "white",
+                    "&:hover": {
+                      backgroundColor: "orange",
+                    },
+                  }}
+                >
+                  Music
+                </Button>
+                <Button size="small">Price : $12</Button>
+              </Box>
+              <Box>
+                <Typography variant="h6">
+                  A Singing competition for every Age
+                </Typography>
+                <Box display="flex" flexDirection="column" marginTop="10px">
+                  <Box display="flex" alignItems="center" gap={2}>
+                    <AccessAlarmIcon />
+                    <Typography variant="p">
+                      Sat, September 10, 2023, 11:00am
+                    </Typography>
+                  </Box>
+                  <Box display="flex" alignItems="center" gap={2}>
+                    <LocationOnIcon />
+                    <Typography variant="p">Paltan Bazar, Guwahati</Typography>
+                  </Box>
+                </Box>
+              </Box>
             </CardContent>
             <CardActions
-              sx={{ display: "flex", justifyContent: "space-between" }}
+              sx={{ display: "flex", justifyContent: "space-around" }}
             >
-              <Button
-                size="small"
-                sx={{ backgroundColor: "red", color: "white" }}
-              >
-                Book Tickets
-              </Button>
-              <Button size="small">Price : $18</Button>
+              <Rating
+                name="simple-controlled"
+                value={value}
+                onChange={(event, newValue) => {
+                  setValue(newValue);
+                }}
+              />
+              <Button sx={{ border: "1px solid black" }}>Book Tickets</Button>
             </CardActions>
           </Card>
           <Card sx={{ maxWidth: 300, maxheight: 400 }}>
-            <CardMedia
-              sx={{ height: 160 }}
-              image={event3}
-              title="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Lifestyle Expo 2023
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Pariatur reiciendis molestiae Lorem ipsum dolor sit amet,
-                consectetur
-              </Typography>
+            <CardMedia sx={{ height: 160 }} image={event3} title="event3" />
+            <CardContent
+              sx={{ display: "flex", flexDirection: "column", gap: 2 }}
+            >
+              <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+                <Button
+                  size="small"
+                  sx={{
+                    backgroundColor: "green",
+                    color: "white",
+                    "&:hover": {
+                      backgroundColor: "orange",
+                    },
+                  }}
+                >
+                  Food And Drink
+                </Button>
+                <Button size="small">Price : $12</Button>
+              </Box>
+              <Box>
+                <Typography variant="h6">
+                  A LifeStyle Expo for every Age
+                </Typography>
+                <Box display="flex" flexDirection="column" marginTop="10px">
+                  <Box display="flex" alignItems="center" gap={2}>
+                    <AccessAlarmIcon />
+                    <Typography variant="p">
+                      Mon, September 5, 2023, 9:00pm
+                    </Typography>
+                  </Box>
+                  <Box display="flex" alignItems="center" gap={2}>
+                    <LocationOnIcon />
+                    <Typography variant="p">IIT Guwahati</Typography>
+                  </Box>
+                </Box>
+              </Box>
             </CardContent>
             <CardActions
-              sx={{ display: "flex", justifyContent: "space-between" }}
+              sx={{ display: "flex", justifyContent: "space-around" }}
             >
-              <Button
-                size="small"
-                sx={{ backgroundColor: "red", color: "white" }}
-              >
-                Book Tickets
-              </Button>
-              <Button size="small">Price : $15</Button>
+              <Rating
+                name="simple-controlled"
+                value={value}
+                onChange={(event, newValue) => {
+                  setValue(newValue);
+                }}
+              />
+              <Button sx={{ border: "1px solid black" }}>Book Tickets</Button>
             </CardActions>
           </Card>
+        </Box>
+      </Box>
+      <Box marginTop="20px" padding="20px 50px">
+        <Typography
+          variant="h5"
+          sx={{
+            textAlign: "center",
+            fontSize: "2rem",
+          }}
+        >
+          How it Work
+        </Typography>
+        <Box
+          sx={{
+            backgroundColor: "red",
+            margin: "auto",
+            height: "3px",
+            width: "100px",
+          }}
+        ></Box>
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          sx={{ flexDirection: { xs: "column", md: "row" }, gap: 10 }}
+          marginTop="50px"
+        >
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            flexDirection="column"
+            width="300px"
+            gap={2}
+          >
+            <Paper
+              elevation={3}
+              sx={{
+                borderRadius: "100px",
+                backgroundColor: "antiquewhite",
+                padding: 3,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "200px",
+              }}
+            >
+              <TravelExploreIcon
+                sx={{ height: "100px", width: "100px", color: "red" }}
+              />
+            </Paper>
+            <Typography variant="h6">Choose What to do</Typography>
+            <Typography variant="p">
+              Easily find your event via search System with multiple params
+            </Typography>
+          </Box>
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            flexDirection="column"
+            width="300px"
+            gap={2}
+          >
+            <Paper
+              elevation={3}
+              sx={{
+                borderRadius: "100px",
+                backgroundColor: "antiquewhite",
+                padding: 3,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "200px",
+              }}
+            >
+              <PublicIcon
+                sx={{ height: "100px", width: "100px", color: "red" }}
+              />
+            </Paper>
+            <Typography variant="h6">Book event that you like</Typography>
+            <Typography variant="p">
+              Choose your event, then book your ticket via payment system
+            </Typography>
+          </Box>
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            flexDirection="column"
+            width="300px"
+            gap={2}
+          >
+            <Paper
+              elevation={3}
+              sx={{
+                borderRadius: "100px",
+                backgroundColor: "antiquewhite",
+                padding: 3,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "200px",
+              }}
+            >
+              <KitesurfingIcon
+                sx={{ height: "100px", width: "100px", color: "red" }}
+              />
+            </Paper>
+            <Typography variant="h6">Get Ticket to Attend</Typography>
+            <Typography variant="p">
+              After Booking You will get ticket in email or you can download it
+              in youe account
+            </Typography>
+          </Box>
         </Box>
       </Box>
     </div>
